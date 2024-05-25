864be1d61e950d28ac0fc97f1e8828defdb8d72d
siam <this.iz.sazzad@gmail.com>
2024-05-26 02:36:30 +0600

    simple change

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,12 +19,12 @@ const Navbar = () => {
                 </span>
             </div>
 
-            <ul className={`absolute md:static left-0 top-[74px] w-full md:w-auto flex flex-col md:flex-row md:items-center py-5 md:py-0 px-6 md:px-0 gap-x-10 gap-y-6 md:gap-y-0 font-primary text-sm bg-white drop-shadow-lg md:drop-shadow-none ${menu ? 'scale-y-100' : 'scale-y-0'} origin-top transition-transform duration-300`}>
+            <ul className={`absolute md:static left-0 top-[74px] w-full md:w-auto flex flex-col md:flex-row md:items-center py-5 md:py-0 px-6 md:px-0 gap-x-10 gap-y-6 md:gap-y-0 font-primary text-sm bg-white drop-shadow-lg md:drop-shadow-none ${menu ? 'scale-y-100' : 'scale-y-0'} md:scale-100 origin-top transition-transform duration-300`}>
                 <a href={'#'} className="navLink">ব্লগ</a>
                 <a href={'#'} className="navLink">বই</a>
                 <a href={'#'} className="navLink">লিখুন</a>
                 <a href={'#'} className="navLink">আমাদের গল্প</a>
-                <button className="bg-gray text-white font-medium px-4 py-2 rounded-3xl hover:bg-primary hover:text-gray transition-all duration-300">শুরু করুন</button>
+                <button className="bg-gray text-white font-medium px-4 py-2 rounded-3xl hover:bg-primary hover:text-gray transition-colors duration-300 ">শুরু করুন</button>
             </ul>
         </nav>
     )
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 
 export default function Home() {
     return (
-        <div>
+        <div className="scroll-smooth">
             <Navbar />
             <Hero />
         </div>
